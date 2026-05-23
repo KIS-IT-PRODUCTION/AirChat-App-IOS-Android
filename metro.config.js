ab215f5b2f1f7607150be2455b52d531dd68bbdd
KIS-IT-PRODUCTION <igorreserv18kis@gmail.com>
2026-05-23 12:08:29 +0300

    fix33

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,29 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 const { transformer, resolver } = config;
 
-// 1. Налаштовуємо трансформер для SVG без повної перезапису об'єкта
+// 1. Налаштування трансформера для SVG
 config.transformer = {
   ...transformer,
   babelTransformerPath: require.resolve('react-native-svg-transformer/expo'), 
 };
 
-// 2. Налаштовуємо резолвер екExtended розширень
+// Шлях до нашої власної локальної заглушки
+const emptyModulePath = path.resolve(__dirname, 'empty-module.js');
+
+// 2. Блокуємо OpenTelemetry за допомогою нашого файлу
 config.resolver = {
   ...resolver,
   assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
   sourceExts: [...resolver.sourceExts, 'svg'],
+  
+  // Перенаправляємо проблемний пакет на наш пустий файл
+  extraNodeModules: {
+    '@opentelemetry/api': emptyModulePath,
+  }
 };
 
 module.exports = config;
